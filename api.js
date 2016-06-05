58c0d47403ed5124eed2e305976fed7ef38147e4
TJ Horner <me@tjhorner.com>
2016-06-05 11:26:52 -0700

    Initial commit

---
+++ api.js
@@ -0,0 +1,61 @@
+var request = require('request'),
+    config = require('./config');
+
+module.exports = function(){
+  var CAH_CREATOR_BASE = "http://localhost:3000/socket_api/";
+
+  var _get = function(endpoint, qs, callback){
+    qs = qs || {};
+    qs.token = config.token;
+    qs.secret = config.secret;
+
+    request(CAH_CREATOR_BASE + endpoint, {
+      qs: qs
+    }, function(err, res, body){
+      if(err){
+        callback(err);
+      }else{
+        callback(null, (typeof(body) === "string" ? JSON.parse(body) : body));
+      }
+    });
+  };
+
+  var _post = function(endpoint, data, callback){
+    var qs = {};
+    qs.token = config.token;
+    qs.secret = config.secret;
+
+    request.post(CAH_CREATOR_BASE + endpoint, {
+      qs: qs,
+      json: true,
+      body: data
+    }, function(err, res, body){
+      if(err){
+        callback(err);
+      }else{
+        callback(null, (typeof(body) === "string" ? JSON.parse(body) : body));
+      }
+    });
+  };
+
+  var getUserWithToken = function(token, callback){
+    _get("user_with_token/" + token, { }, callback);
+  };
+
+  var verifyAccessToDeck = function(token, deckId, callback){
+    _get("has_access_to_deck/" + token + "/" + deckId, { }, callback);
+  };
+
+  var saveDeck = function(deckId, newDeck, callback){
+    _post("save_deck", {
+      deck_id: deckId,
+      new_deck: newDeck
+    }, callback);
+  };
+
+  return {
+    getUserWithToken: getUserWithToken,
+    verifyAccessToDeck: verifyAccessToDeck,
+    saveDeck: saveDeck
+  };
+};
---
+++ app.js
@@ -0,0 +1,138 @@
+// TODO literally all the server-side code is in one file.
+// I should probably split these up more. possibly use
+// actual models for the decks?
+
+var express = require('express'),
+    app = express(),
+    api = require('./api')(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    sessions = { };
+
+// app.use(express.static(__dirname + "/public"));
+
+var randId = function(){
+  var id = "";
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for(var i=0; i < 10; i++) id += chars.charAt(Math.floor(Math.random() * chars.length));
+
+  return id;
+};
+
+io.on("connection", function(socket){
+  socket.emit("hello");
+
+  socket.on("authenticate", function(token){
+    api.getUserWithToken(token, function(err, res){
+      if(!err){
+        socket.token = token;
+        socket.authenticated = true;
+        socket.emit("authenticated", res);
+      }
+    });
+  });
+
+  socket.on("authenticate:session", function(auth){
+    if(sessions[auth.sessionId] && sessions[auth.sessionId].token === auth.sessionToken){
+      socket.session = sessions[auth.sessionId];
+      socket.sessionId = auth.sessionId;
+      socket.join(auth.sessionId);
+
+      socket.emit("session", {
+        sessionId: auth.sessionId,
+        sessionToken: auth.sessionToken,
+        deck: socket.session.deck
+      });
+    }
+  });
+
+  socket.on("deck:load", function(id){
+    api.verifyAccessToDeck(socket.token, id, function(err, res){
+      if(res.has_access && res.deck){
+        var sessionId = randId(),
+            sessionToken = randId();
+
+        sessions[sessionId] = {
+          token: sessionToken,
+          deck: res.deck
+        };
+
+        socket.session = sessions[sessionId];
+        socket.sessionCreator = true;
+        socket.sessionId = sessionId;
+        socket.join(sessionId);
+
+        socket.emit("session", {
+          sessionId: sessionId,
+          sessionToken: sessionToken,
+          deck: socket.session.deck
+        });
+      }
+    });
+  });
+
+  socket.on("deck:card:black", function(card){
+    if(socket.session && parseInt(card.pick) !== NaN && parseInt(card.pick) > 0 && card.text.trim() !== "" && card.pick.trim() !== ""){
+      socket.session.deck.black_cards.push({ text: card.text, pick: parseInt(card.pick) });
+      socket.to(socket.sessionId).emit("deck:card:black", { text: card.text, pick: parseInt(card.pick) });
+      socket.emit("deck:card:black", { text: card.text, pick: parseInt(card.pick) });
+    }
+  });
+
+  socket.on("deck:card:white", function(card){
+    if(socket.session && card.trim() !== ""){
+      socket.session.deck.white_cards.push(card);
+      socket.to(socket.sessionId).emit("deck:card:white", card);
+      socket.emit("deck:card:white", card);
+    }
+  });
+
+  socket.on("deck:card:black:edit", function(card){
+    if(socket.session && socket.session.deck.black_cards[card.index] && parseInt(card.pick) !== NaN && parseInt(card.pick) > 0 && card.text.trim() !== "" && card.pick.trim() !== ""){
+      socket.session.deck.black_cards[card.index] = { text: card.text, pick: parseInt(card.pick) };
+      socket.to(socket.sessionId).emit("deck:card:black:edit", card);
+      socket.emit("deck:card:black:edit", card);
+    }
+  });
+
+  socket.on("deck:card:white:edit", function(card){
+    if(socket.session && socket.session.deck.white_cards[card.index] && card.text.trim() !== ""){
+      socket.session.deck.white_cards[card.index] = card.text;
+      socket.to(socket.sessionId).emit("deck:card:white:edit", card);
+      socket.emit("deck:card:white:edit", card);
+    }
+  });
+
+  socket.on("deck:save", function(){
+    if(socket.session && socket.sessionCreator){
+      api.saveDeck(socket.session.deck.id, socket.session.deck, function(err, res){
+        if(!err){
+          socket.to(socket.sessionId).emit("session:end");
+          socket.emit("session:end");
+          sessions[socket.sessionId] = undefined;
+        }
+      });
+    }
+  });
+
+  socket.on("deck:name", function(name){
+    if(socket.session && socket.session.deck && name.trim() !== ""){
+      socket.session.deck.name = name;
+      socket.to(socket.sessionId).emit("deck:name", name);
+    }
+  });
+
+  socket.on("disconnect", function(){
+    if(socket.sessionCreator){
+      socket.to(socket.sessionId).emit("session:end");
+      sessions[socket.sessionId] = undefined;
+    }
+  });
+});
+
+app.get("/", function(req, res){
+  res.redirect("https://cahcreator.com/");
+});
+
+http.listen(process.env.PORT || 8080);
